258b81abf56453309e3877d7b8ecefa408e4ce06
AhmedMohamed800 <ahmedmoh0107@gmail.com>
2026-04-08 19:03:21 +0200

    chore: create boiler plate for commander

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
- 
+#!/usr/bin/env node
+
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .name("wcag-checker-cli")
+  .description("Fast, developer-first accessibility testing for WCAG 2.2")
+  .version("0.0.1");
+
+program.parse(process.argv);
